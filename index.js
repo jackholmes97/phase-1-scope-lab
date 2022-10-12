0782e3c3baffeb673a0fac65ff0cd27b6a801734
Jack Holmes <jdh97@comcast.net>
2022-10-12 12:57:42 -0400

    commit tree 51dd40d74ab26deb7db1759ef63c2b74f73ddcfd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = `bob`;
+const leastFavoriteCustomer = 'steve';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`;
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = `not steve`;
+}
